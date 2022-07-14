de25a8fc6898dab93928fdf826a1b527c84bb0a4
McKenzie <mckenzielynn@ibm.com>
2022-07-14 15:09:33 -0500

    Adding delete button

--- src/App.js
+++ src/App.js
@@ -4,43 +4,52 @@ import NotesList from "./components/NotesList";
 
 
 const App = () => {
-	const [notes, setNotes] = useState([
-		{
-			id: nanoid(),
-			text: 'This is my first note!',
-			date: '15/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my second note!',
-			date: '21/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my third note!',
-			date: '28/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my new note!',
-			date: '30/04/2021',
-		},
-	]);
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note!',
+      date: '15/04/2021',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my second note!',
+      date: '21/04/2021',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my third note!',
+      date: '28/04/2021',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my new note!',
+      date: '30/04/2021',
+    },
+  ]);
 
   const addNote = (text) => {
-      const date = new Date();
-      const newNote = {
-        id: nanoid(),
-        text: text,
-        date: date.toLocaleDateString() /* Converts date to locally understood language */
-      }
-      const newNotes = [...notes, newNote];
-      setNotes(newNotes);
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString() /* Converts date to locally understood language */
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
   }
 
   return (
     <div className='container'>
-        <NotesList notes={notes} handleAddNote={addNote}/>
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
 };
